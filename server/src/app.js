9b725384544b8c65d4e8b99010399b42dcfe302a
AnMaro <marooo.anastasita@gmail.com>
2026-05-14 23:09:43 +0300

    auth

--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const apiRouter = require('./routes/apiRoute');
-const viewRouter = require('./routes/viewRoute');
-const serverConfig = require('./config/serverConfig');
-const http = require('http');
-const initChatSocket = require('./ws/chatSocket');
+const express = require("express");
+const apiRouter = require("./routes/apiRoute");
+
+const serverConfig = require("./config/serverConfig");
+const http = require("http");
+const initChatSocket = require("./ws/chatSocket");
 
 const PORT = process.env.PORT ?? 3000;
 const app = express();
 
 serverConfig(app);
 
-app.use('/api', apiRouter);
-app.use('/', viewRouter);
+app.use("/api", apiRouter);
 
 const server = http.createServer(app);
 initChatSocket(server);
