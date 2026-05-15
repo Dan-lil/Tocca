5448cf6fa0de908bf89fad28fcf74814c8e3ca9c
OliverBrave <lelek9733@gmail.com>
2026-05-15 21:43:37 +1000

    NotFoundPage 0.0.2

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,13 @@
 
-export default function HomePage() {
-  return (
-    <div>page</div>
-  )
+// export default function HomePage() {
+//   return (
+//     <div>page</div>
+//   )
+// }
+
+import NotFoundPage from '@/app/NotFoundPage/not-found';
+
+export default function Home() {
+  // Временно показываем страницу 404 на главной
+  return <NotFoundPage />;
 }
