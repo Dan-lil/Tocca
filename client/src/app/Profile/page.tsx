5973225ad2feab306419e8c8c60e8f209d649751
OliverBrave <lelek9733@gmail.com>
2026-05-21 21:09:50 +1000

    en

--- client/src/app/Profile/page.tsx
+++ client/src/app/Profile/page.tsx
@@ -4,7 +4,6 @@ import "./page.css";
 import Link from "next/link";
 import { useLocale, useTranslations } from "next-intl";
 import { useRouter } from "next/navigation";
-import { useTranslations } from "next-intl";
 import { type FormEvent, useEffect, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { io } from "socket.io-client";
